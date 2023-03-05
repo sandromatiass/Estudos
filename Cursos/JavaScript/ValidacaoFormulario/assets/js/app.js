ff4dadd3668d9de86cc324e158b37214cd2cdb5c
Sandro Matias <devsandromatias@gmail.com>
2023-03-04 22:10:48 -0300

    Iniciando validação de cpf no formulário.

--- Cursos/JavaScript/ValidacaoFormulario/assets/js/app.js
+++ Cursos/JavaScript/ValidacaoFormulario/assets/js/app.js
@@ -1,14 +1,10 @@
-//importando apenas a função de um documento js
 import { valida } from './validacao.js'
 
-//aqui eu seleciono todos os imputs por isso utiliza o querySelectorAll("usa seletor de tag no caso o input")
 const inputs = document.querySelectorAll('input')
 
-//criando um laço de repetição
-//para cada input que nos temos criamos o evento de blur e a função valida passando por todos os inputs
+
 inputs.forEach(input => {
     input.addEventListener('blur', (evento) =>{
-        //aqui chama a função valida junto com evento de target
         valida(evento.target)
     })
 })
