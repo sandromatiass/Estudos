1cb58f9137003cd94fac9f0e6860fd7c27b7d994
Sandro Matias <devsandromatias@gmail.com>
2023-03-06 23:10:50 -0300

    Algumas alterações realizadas o códido e criação de modules

--- Cursos/JavaScript/ProgramandoOrientacaoObjetos/index.js
+++ Cursos/JavaScript/ProgramandoOrientacaoObjetos/index.js
@@ -1,28 +1,26 @@
-//trabalhando com class
-//1- criando uma class
-class Cliente{
-    nome;
-    cpf;
-    agencia;
-    saldo;
-    rg;
-}
+import { Cliente } from "./Cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
 
-//criando uma variável para novo cliente
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
-//criando propiedade para o cliente
 cliente1.nome = "Sandro";
 cliente1.cpf =  11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
-cliente1.rg = 12543688;
 
-
+const cliente2 = new Cliente();
 cliente2.nome = "Neto";
 cliente2.cpf =  88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+
+const contaCorrenteSandro = new ContaCorrente();
+contaCorrenteSandro._saldo = 0;
+contaCorrenteSandro.agencia = 1001;
+contaCorrenteSandro.cliente = cliente1;
+contaCorrenteSandro.depositar(500);
+
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
+
+contaCorrenteSandro.transferir(200, conta2);
+
+
+console.log(conta2);
