0063f711f5eafa97a3f6b39f836d07573ae22076
Sandro Matias NoteBook <devsandromatias@gmail.com>
2023-03-07 15:17:44 -0300

    Organizando pastas

--- Cursos/JavaScript - Matheus Batisti/aulas/7_boleans/script.js
+++
@@ -1,4 +0,0 @@
-console.log(5 > 2)
-console.log(3 > 10)
-console.log(true)
-console.log(false)
---
+++ Cursos/JavaScriptMatheusBatisti/Aulas/modulo1/7_boleans/script.js
@@ -0,0 +1,4 @@
+console.log(5 > 2)  //return true
+console.log(3 > 10) //return false
+console.log(true)
+console.log(false)
