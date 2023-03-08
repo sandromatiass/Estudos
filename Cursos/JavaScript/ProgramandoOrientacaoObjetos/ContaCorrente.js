d5eadeade9cdbf7e131d49752032151a2b103499
Sandro Matias <devsandromatias@gmail.com>
2023-03-07 23:45:31 -0300

    Finalizado Curso JavaScript programando a Orientação a Objetos

--- Cursos/JavaScript/ProgramandoOrientacaoObjetos/ContaCorrente.js
+++ Cursos/JavaScript/ProgramandoOrientacaoObjetos/ContaCorrente.js
@@ -1,8 +1,31 @@
-export class ContaCorrente {
-    agencia;
-    cliente;
+import { Cliente } from "./Cliente.js";
 
+export class ContaCorrente {
+    //atributo estatico
+    static numeroDeContas = 0;
+    agencia;
+    _cliente;
     _saldo = 0;
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        } 
+    }
+
+    get saldo(){
+        return this.saldo;
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    constructor(agencia, cliente){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
 
     sacar(valor){
         if(this._saldo >= valor){
@@ -17,6 +40,7 @@ export class ContaCorrente {
         }
         this._saldo += valor;
     }
+    
     transferir(valor, conta){
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
--- Cursos/JavaScript/ProgramandoOrientacaoObjetos/index.js
+++ Cursos/JavaScript/ProgramandoOrientacaoObjetos/index.js
@@ -1,26 +1,16 @@
 import { Cliente } from "./Cliente.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = "Sandro";
-cliente1.cpf =  11122233309;
+const cliente1 = new Cliente("Sandro", 12313233111);
+const cliente2 = new Cliente("Neto", 88822233309);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Neto";
-cliente2.cpf =  88822233309;
-
-
-const contaCorrenteSandro = new ContaCorrente();
-contaCorrenteSandro._saldo = 0;
-contaCorrenteSandro.agencia = 1001;
-contaCorrenteSandro.cliente = cliente1;
+const contaCorrenteSandro = new ContaCorrente(1001, cliente1);
 contaCorrenteSandro.depositar(500);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 102;
-
-contaCorrenteSandro.transferir(200, conta2);
+const conta2 = new ContaCorrente(102, cliente2);
 
 
-console.log(conta2);
+let valor = 200;
+contaCorrenteSandro.transferir(valor, conta2)
+
+console.log(ContaCorrente.numeroDeContas);
