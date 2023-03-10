1d83f185df6c2ec3877f96c48e95b4c5c415865c
Sandro Matias <devsandromatias@gmail.com>
2023-03-09 22:06:57 -0300

    Inciando 2_Modulo JavaScript Matheus Batisti

---
+++ Cursos/JavaScriptMatheusBatisti/Aulas/modulo2/14_variaveis/script.js
@@ -0,0 +1,11 @@
+let nome = "Sandro";
+let idade = 15;
+let cidade = "Maceió";
+
+console.log(`Meu nome é ${nome} tenho ${idade} e moro em ${cidade}`);
+
+nome = "Jardane";
+idade = 28;
+cidade = "União";
+
+console.log(`Meu nome é ${nome} tenho ${idade} e moro em ${cidade}`);
