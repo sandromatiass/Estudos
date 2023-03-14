d7ba728760e3e8dcaeada21e9be5c13b052a4ca1
Sandro Matias <devsandromatias@gmail.com>
2023-03-13 21:57:14 -0300

    Finalizando Curso Matheus Batisti

--- Cursos/JavaScriptMatheusBatisti/Aulas/modulo2/23_estruturas_de_repetição_while/script.js
+++ Cursos/JavaScriptMatheusBatisti/Aulas/modulo2/23_estruturas_de_repetição_while/script.js
@@ -4,6 +4,11 @@ while(x > 0){
     x = x - 1
 }
 
+//^--x é igual a 10
+//enquanto x for maior que 0 executar o codigo abaixo na codição esta informando
+//x = x -1 (10 menos 1 até chegar a zero)
+//contagem regressiva
+
 console.log("aqui termina e começa")
 
 let y = 0
@@ -12,3 +17,7 @@ while(y <= 10){
     y += 1
 
 }
+
+//^--y é igual a 0
+//enquando y for menor que 10 executar o código abaixo
+//y += 1 (diz que a cada verificação o valor de y será adicionado mais 1)
