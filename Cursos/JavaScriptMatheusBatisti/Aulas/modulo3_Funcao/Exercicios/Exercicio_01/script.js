831c84aa6f23737914e59b32714a87088901ef04
Sandro Matias NoteBook <devsandromatias@gmail.com>
2023-03-23 16:49:17 -0300

    Iniciando estudos

---
+++ Cursos/JavaScriptMatheusBatisti/Aulas/modulo3_Funcao/Exercicios/Exercicio_01/script.js
@@ -0,0 +1,4 @@
+function mensagem (){
+    console.log('Hello World!');
+}
+mensagem();
---
+++ Cursos/JavaScriptMatheusBatisti/Aulas/modulo3_Funcao/Exercicios/Exercicio_02/script.js
@@ -0,0 +1,12 @@
+function informeSuaIdade(idade) {
+    if(idade <= 1){
+        console.log(`Sua idade é ${idade} ano.`);
+    } else {
+        console.log(`Sua idade é ${idade} anos.`);
+    }
+}
+
+informeSuaIdade(28);
+informeSuaIdade(28);
+informeSuaIdade(8);
+informeSuaIdade(1)
