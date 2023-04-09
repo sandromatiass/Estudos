45c94f77aba6714138334368cdc08e7a3fe0e5b3
Sandro Matias <devsandromatias@gmail.com>
2023-04-08 21:34:30 -0300

    Finalizando curso JavaScript focado em CRUD - Alura

--- Cursos/JavaScriptAlura/JSnaWebCRUD/projeto_inicial/admin/service/client-service.js
+++ Cursos/JavaScriptAlura/JSnaWebCRUD/projeto_inicial/admin/service/client-service.js
@@ -1,45 +1,76 @@
-const criaNovaLinha = (nome, email) => {
-    const linhaNovoCliente = document.createElement('tr')
-    
-    const conteudo = `
-    
-    <td class="td" data-td>${nome}</td>
-    <td>${email}</td>
-    <td>
-        <ul class="tabela__botoes-controle">
-            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-        </ul>
-    </td> 
-
-    `
-    linhaNovoCliente.innerHTML = conteudo
-    return linhaNovoCliente
-}
-
-const tabela = document.querySelector('[data-tabela]')
-
 const listaClientes = () => {
-    const promise = new Promise((resolve, reject) => {
-        const http = new XMLHttpRequest()
-        http.open('GET', 'http://localhost:3000/profile')
-    
-        http.onload = () => {
-            if(http.status >= 400){
-                reject(JSON.parse(http.response));
-            }else {
-                resolve(JSON.parse(http.response));
-            }
+    return fetch(`http://localhost:3000/profile`)
+    .then( resposta => {
+        if(resposta.ok){
+            return resposta.json()
         }
-        http.send();
+        throw new Error('Não foi possível listar os clientes.')
     })
-    return promise
 }
 
-
-listaClientes()
-.then(data =>{
-    data.forEach(elemento => {
-    tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))    
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        }) 
     })
-})
+    .then( resposta => {
+        if(resposta.ok){
+            return resposta.body
+        }
+        throw new Error('Não foi possível criar o clientes.')
+    })
+}
+
+const removeCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    }).then(resposta => {
+        if(!resposta.ok){
+            throw new Error('Não foi possível remover o clientes.')
+        }
+    })
+}
+
+const detalhaCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`)
+    .then(resposta => {
+        if(resposta.ok){
+            return resposta.json()   
+        }
+        throw new Error('Não foi possível detalhar o clientes.')
+    })
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type' : 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+    .then( resposta => {
+        if(resposta.ok){
+            return resposta.json()
+        }
+        throw new Error('Não foi possível atualizar clientes.')
+    })
+}
+
+export const clienteService = {
+    listaClientes,
+    criaCliente,
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
+}
+
